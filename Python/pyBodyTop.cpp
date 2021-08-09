71889bc0604074b2d20083cadf18993a436abec8
Jared Duffey <jared.duffey@bluequartz.net>
2021-08-09 13:33:55 -0400

    Added python binding for FloatVec21_t
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

---
+++ Python/pyBodyTop.cpp
@@ -0,0 +1,3 @@
+py::class_<FloatVec21_t> instanceFloatVec21(mod, "FloatVec21");
+
+instanceFloatVec21.def(py::init<float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float, float>());
